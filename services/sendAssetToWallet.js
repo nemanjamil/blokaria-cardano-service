c828861236681bdb4fbe76d35b31ed55419372e4
mihajlo1604 <mihajlo_sk95@hotmail.com>
2022-06-08 21:51:12 +0200

    minor fix

--- services/sendAssetToWallet.js
+++ services/sendAssetToWallet.js
@@ -1,147 +1,136 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const cardano = require("../config/cardano")
-const Joi = require('joi');
+const cardano = require("../config/cardano");
+const Joi = require("joi");
 
 const bodySchema = Joi.object({
-    walletName: Joi.string().min(5).max(100).required(),
-    addressWallet: Joi.string().min(5).max(300).required(),
-    assetId: Joi.string().min(5).max(200).required(),
-    amountValue: Joi.number().required()
-})
+  walletName: Joi.string().min(5).max(100).required(),
+  addressWallet: Joi.string().min(5).max(300).required(),
+  assetId: Joi.string().min(5).max(200).required(),
+  amountValue: Joi.number().required(),
+});
 
 router.use((req, res, next) => {
-    console.log('Time sendAssetToWallet: ', Date.now(), '\n\n')
-    next()
-})
+  console.log("Time sendAssetToWallet: ", Date.now(), "\n\n");
+  next();
+});
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  const { body } = req;
 
-    const { body } = req;
+  console.log("\n\n sendAssetToWallet START \n\n");
+  console.log("SendAssetToWallet Payload ", body);
 
-    console.log('\n\n sendAssetToWallet START \n\n')	
-    console.log("SendAssetToWallet Payload ", body)
+  try {
+    const value = await bodySchema.validateAsync(body);
 
-    try {
+    let addressWallet = body.addressWallet;
+    let walletName = body.walletName;
+    let assetId = body.assetId;
+    //let amountValue = body.amountValue;
+    const amountValue = 1;
 
-        const value = await bodySchema.validateAsync(body);
+    const sender = cardano.wallet(walletName);
 
-        let addressWallet = body.addressWallet
-        let walletName = body.walletName
-        let assetId = body.assetId
-	let amountValue = body.amountValue
+    let walletBalance = sender.balance();
 
-        const sender = cardano.wallet(walletName)
+    console.log("walletBalance", walletBalance);
 
-        let walletBalance = sender.balance();
+    let getAllData = walletBalance.utxo[0].value;
 
-        console.log("walletBalance", walletBalance);
+    const numberOfAssets = Object.keys(getAllData).length;
 
-	let getAllData = walletBalance.utxo[0].value;
+    console.log("numberOfAssets", numberOfAssets);
 
-        const numberOfAssets  = Object.keys(getAllData).length;
+    delete getAllData.lovelace;
+    delete getAllData.undefined;
+    delete getAllData[assetId];
 
-	console.log("numberOfAssets", numberOfAssets);
+    console.log("Sender wallet name ", walletName);
+    console.log(
+      "Balance of Sender wallet: " +
+        cardano.toAda(sender.balance().value.lovelace) +
+        " ADA"
+    );
 
-        delete getAllData.lovelace
-	delete getAllData.undefined
-	delete getAllData[assetId]
+    const receiver = addressWallet;
 
-	console.log("Sender wallet name ", walletName)
-        console.log(
-            "Balance of Sender wallet: " +
-            cardano.toAda(sender.balance().value.lovelace) + " ADA"
-        )
+    //let amountValue = 1.5
 
-        const receiver = addressWallet
+    const txInfo = {
+      txIn: cardano.queryUtxo(sender.paymentAddr),
+      txOut: [
+        {
+          address: sender.paymentAddr,
+          value: {
+            lovelace:
+              sender.balance().value.lovelace - cardano.toLovelace(amountValue),
+          },
+        },
+        {
+          address: receiver,
+          value: {
+            lovelace: cardano.toLovelace(amountValue),
+            [assetId]: 1,
+          },
+        },
+      ],
+    };
 
-        //let amountValue = 1.5
+    console.log("Original txInfo ", txInfo);
 
-	const txInfo = {
-            txIn: cardano.queryUtxo(sender.paymentAddr),
-            txOut: [
-                {
-                    address: sender.paymentAddr,
-                    value: {
-                        lovelace: sender.balance().value.lovelace - cardano.toLovelace(amountValue),
-                    }
-                },
-                {
-                    address: receiver,
-                    value: {
-                        lovelace: cardano.toLovelace(amountValue),
-                        [assetId]:1 
-                    }
-                }
-            ]
-        }
-	
-	console.log("Original txInfo ", txInfo)
-
-
-	if (numberOfAssets>2) {
-		console.log("Entered Update My Wallet")
-		Object.assign(txInfo.txOut[0].value, getAllData);
-	}
-
-        console.log("SendAssetToWallet txInfo: ", JSON.stringify(txInfo, null, 4));
-
-        // 3. build the transaction
-        const raw = cardano.transactionBuildRaw(txInfo)
-        console.log("SendAssetToWallet raw ", raw)
-
-        // 4. calculate the fee
-        const fee = cardano.transactionCalculateMinFee({
-            ...txInfo,
-            txBody: raw,
-            witnessCount: 1
-        })
-
-        console.log("SendAssetToWallet fee ", fee)
-
-        // 5. pay the fee by subtracting it from the sender utxo
-
-        // txInfo.txOut[0].value.lovelace -= fee
-        //console.log(JSON.stringify(txInfo, null, 4));
-
-	// 6. build the final transaction
-	
-	let spreadTxInfo = {...txInfo}
-
-	spreadTxInfo.txOut[0].value.lovelace -= fee
-
-	console.log(JSON.stringify(spreadTxInfo, null, 4));
-
-        const tx = cardano.transactionBuildRaw({ ...spreadTxInfo, fee })
-
-        console.log("SendAssetToWallet tx ", tx)
-        // 7. sign the transaction
-
-        const txSigned = cardano.transactionSign({
-            txBody: tx,
-            signingKeys: [sender.payment.skey]
-        })
-
-        console.log("SendAssetToWallet txSigned ", txSigned)
-        // 8. submit the transaction
-
-        const txHash = cardano.transactionSubmit(txSigned)
-
-	console.log("SendAssetToClient FINISH")
-        res.json({ txHash })
-        console.log("SendAssetToWallet ", txHash)
-
-    }
-    catch (err) {
-        return res.status(400).json({ error: err.toString() });
+    if (numberOfAssets > 2) {
+      console.log("Entered Update My Wallet");
+      Object.assign(txInfo.txOut[0].value, getAllData);
     }
 
-})
+    console.log("SendAssetToWallet txInfo: ", JSON.stringify(txInfo, null, 4));
+
+    // 3. build the transaction
+    const raw = cardano.transactionBuildRaw(txInfo);
+    console.log("SendAssetToWallet raw ", raw);
+
+    // 4. calculate the fee
+    const fee = cardano.transactionCalculateMinFee({
+      ...txInfo,
+      txBody: raw,
+      witnessCount: 1,
+    });
+
+    console.log("SendAssetToWallet fee ", fee);
+
+    // 5. pay the fee by subtracting it from the sender utxo
+
+    // txInfo.txOut[0].value.lovelace -= fee
+    //console.log(JSON.stringify(txInfo, null, 4));
+
+    // 6. build the final transaction
+
+    txInfo.txOut[0].value.lovelace -= fee;
+
+    console.log(JSON.stringify(txInfo, null, 4));
+
+    const tx = cardano.transactionBuildRaw({ ...txInfo, fee });
+
+    console.log("SendAssetToWallet tx ", tx);
+    // 7. sign the transaction
+
+    const txSigned = cardano.transactionSign({
+      txBody: tx,
+      signingKeys: [sender.payment.skey],
+    });
+
+    console.log("SendAssetToWallet txSigned ", txSigned);
+    // 8. submit the transaction
+
+    const txHash = cardano.transactionSubmit(txSigned);
+
+    console.log("SendAssetToClient FINISH");
+    res.json({ txHash });
+    console.log("SendAssetToWallet ", txHash);
+  } catch (err) {
+    return res.status(400).json({ error: err.toString() });
+  }
+});
 
 module.exports = router;
-
-
-
-
-
-
