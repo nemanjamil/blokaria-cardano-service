b929a6d9a28a6f45a93e03edf7eedb0f76c65720
Nemanja Mili <nemanjamil@gmail.com>
2022-06-18 21:35:00 +0200

    CARDANO_TESTNET

--- config/cardano.js
+++ config/cardano.js
@@ -1,7 +1,10 @@
 const Cardano = require("cardanocli-js")
 
+console.log('process.env.CARDANO_TESTNET', process.env.CARDANO_TESTNET);
+
 const cardano = new Cardano({
-    network: "testnet-magic 1097911063",
+    //network: "testnet-magic 1097911063",
+    network: process.env.CARDANO_TESTNET,
     dir: "/opt/cardano/cnode",
     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
 });
