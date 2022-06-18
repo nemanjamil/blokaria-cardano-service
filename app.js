35ca40ee866a1c0c765717937d8511431ecd9cc3
Nemanja Mili <nemanjamil@gmail.com>
2022-06-18 22:35:56 +0200

    Add Process

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var getBalance = require('./services/getBalance')
 var checkWallet = require('./services/checkWallet')
 //var createWallet = require('./services/createWallet')
 
+console.log('\n\n');
+console.log('KRENUO PROCESS');
 console.log('APP process.env.CARDANO_NET_MAGIC', process.env.CARDANO_NET_MAGIC);
 
 app.use(express.json())
