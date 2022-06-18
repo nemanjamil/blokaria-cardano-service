1433c4e84d2144beea5fc29c596c227c70bc4dd4
Nemanja Mili <nemanjamil@gmail.com>
2022-06-18 21:38:06 +0200

    CARDANO_NET_MAGIC

--- config/cardano.js
+++ config/cardano.js
@@ -1,6 +1,6 @@
 const Cardano = require("cardanocli-js")
 
-console.log('process.env.CARDANO_TESTNET', process.env.CARDANO_TESTNET);
+console.log('process.env.CARDANO_NET_MAGIC', process.env.CARDANO_NET_MAGIC);
 
 const cardano = new Cardano({
     //network: "testnet-magic 1097911063",
