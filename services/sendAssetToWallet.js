befa95a7d86990f91d24881d481428847d84dd81
mihajlo1604 <35923303+mihajlo1604@users.noreply.github.com>
2022-06-25 18:59:00 +0200

    parse amount val

--- services/sendAssetToWallet.js
+++ services/sendAssetToWallet.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
     let addressWallet = body.addressWallet;
     let walletName = body.walletName;
     let assetId = body.assetId;
-    let amountValue = body.amountValue;
+    let amountValue = parseInt(body.amountValue);
     console.log("amountValue ", amountValue);
     //const amountValue = 1;
 
