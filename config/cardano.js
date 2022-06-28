89ea42b818bf8229f4d4218c3eea57c9116046e7
Nemanja Mili <nemanjamil@gmail.com>
2022-06-28 20:18:28 +0200

    Add MAIN_TEST_MAGIC

--- config/cardano.js
+++ config/cardano.js
@@ -1,15 +1,9 @@
 const Cardano = require("cardanocli-js")
 
 const cardano = new Cardano({
-    //network: "testnet-magic 1097911063",
-    //network: "mainnet-magic 764824073",
     network: process.env.CARDANO_NET_MAGIC,
     dir: "/opt/cardano/cnode",
     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
 });
-
-
-//const shelleyGenesisPath = "/opt/cardano/cnode/files/shelley-genesis.json";
-//const cardano  = new Cardano({ shelleyGenesisPath });
 
 module.exports = cardano;
