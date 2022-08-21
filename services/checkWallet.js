3e7a1ee809362a1257859beb6aa9265b4e30dcc9
Nemanja Mili <nemanjamil@gmail.com>
2022-08-21 20:10:54 +0200

    stringify

--- services/checkWallet.js
+++ services/checkWallet.js
@@ -25,6 +25,7 @@ router.post('/', async (req, res) => {
         const walletscript = cardano.wallet(walletName)
 
         console.log("Prosao WalletScript", walletscript)
+        console.log("Prosao WalletScript stringify", JSON.stringify(walletscript, null, 4))
 
         res.json({
             "balance": walletscript.balance(),
