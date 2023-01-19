8e4dc6601b99f193a34349158375399d48509976
Nemanja Mili <nemanjamil@gmail.com>
2023-01-19 13:18:58 +0100

    764824073

--- config/cardano.js
+++ config/cardano.js
@@ -1,7 +1,7 @@
 const Cardano = require("cardanocli-js")
 
 const cardano = new Cardano({
-    network: "mainnet",
+    network: "mainnet 764824073",
     dir: "/opt/cardano/cnode",
     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
 });
