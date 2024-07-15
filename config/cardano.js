b69f36a58b299e38bb8bf11dccd45f3455194049
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-15 08:32:07 +0200

    Update to 4.1.6

--- config/cardano.js
+++ config/cardano.js
@@ -1,13 +1,19 @@
-const { CardanoCliJs, CardanoCliJsOptions } = require("cardanocli-js");
-const shelleyGenesisPath = process.env.SHELLEY_GENESIS_PATH;
-const options = new CardanoCliJsOptions({
-  shelleyGenesisPath,
-  network: 1097911063,
-});
-const cardano = new CardanoCliJs(options);
-
+// const { CardanoCliJs, CardanoCliJsOptions } = require("cardanocli-js");
+// const shelleyGenesisPath = process.env.SHELLEY_GENESIS_PATH;
+// const options = new CardanoCliJsOptions({
+//   shelleyGenesisPath,
+//   network: 1097911063,
+// });
+// const cardano = new CardanoCliJs(options);
+// module.exports = cardano;
 // const cardano = new Cardano({
 //     network: process.env.CARDANO_NET_MAGIC,
 //     dir: "/opt/cardano/cnode",
 //     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
 // });
+
+const CardanocliJs = require("cardanocli-js");
+const shelleyGenesisPath = process.env.SHELLEY_GENESIS_PATH;
+
+const cardano = new CardanocliJs({ shelleyGenesisPath });
+module.exports = cardano;
