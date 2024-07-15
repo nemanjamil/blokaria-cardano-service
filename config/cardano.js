1b3eeeb10a1276fa38fbb8a3218c7ef98e90c3f1
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-15 08:57:11 +0200

    Remove comments

--- config/cardano.js
+++ config/cardano.js
@@ -1,17 +1,3 @@
-// const { CardanoCliJs, CardanoCliJsOptions } = require("cardanocli-js");
-// const shelleyGenesisPath = process.env.SHELLEY_GENESIS_PATH;
-// const options = new CardanoCliJsOptions({
-//   shelleyGenesisPath,
-//   network: 1097911063,
-// });
-// const cardano = new CardanoCliJs(options);
-// module.exports = cardano;
-// const cardano = new Cardano({
-//     network: process.env.CARDANO_NET_MAGIC,
-//     dir: "/opt/cardano/cnode",
-//     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
-// });
-
 const CardanocliJs = require("cardanocli-js");
 const shelleyGenesisPath = process.env.SHELLEY_GENESIS_PATH;
 const cardano = new CardanocliJs({
