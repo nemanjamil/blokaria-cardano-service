2a5bfb9bd953edee11a93ab4ed20d4fb1c72317c
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-15 07:36:32 +0200

    Update Cardano JS

--- config/cardano.js
+++ config/cardano.js
@@ -1,9 +1,11 @@
-const Cardano = require("cardanocli-js")
-
-const cardano = new Cardano({
-    network: process.env.CARDANO_NET_MAGIC,
-    dir: "/opt/cardano/cnode",
-    shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
-});
+const CardanocliJs = require("cardanocli-js");
+const shelleyGenesisPath =
+  "/opt/cardano/cnode/files/mainnet-shelley-genesis.json";
+const cardano = new CardanocliJs({ shelleyGenesisPath });
+// const cardano = new Cardano({
+//     network: process.env.CARDANO_NET_MAGIC,
+//     dir: "/opt/cardano/cnode",
+//     shelleyGenesisPath: "/opt/cardano/cnode/files/shelley-genesis.json"
+// });
 
 module.exports = cardano;
