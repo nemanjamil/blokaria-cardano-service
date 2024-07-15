4c1ec48f9db9c0c1e0b310b2095b92ae5b83dd4a
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-15 08:49:08 +0200

    Add socketPath

--- config/cardano.js
+++ config/cardano.js
@@ -18,5 +18,6 @@ const cardano = new CardanocliJs({
   network: process.env.CARDANO_NET_MAGIC,
   dir: "/opt/cardano/cnode",
   shelleyGenesisPath,
+  socketPath: "/opt/cardano/cnode/sockets",
 });
 module.exports = cardano;
