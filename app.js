ce4d052ab90b49b7309a5098ae4a966a715bb7cb
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-16 16:44:23 +0200

    Change healthCheck to get

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
-const express = require('express')
-const app = express()
-const port = 3333
-require('dotenv').config();
-var generateNFT = require('./services/generateNFT');
-var sendAssetToWallet = require('./services/sendAssetToWallet');
-var getBalance = require('./services/getBalance')
-var checkWallet = require('./services/checkWallet')
-var walletToWalletTx = require('./services/walletToWalletTx')
+const express = require("express");
+const app = express();
+const port = 3333;
+require("dotenv").config();
+var generateNFT = require("./services/generateNFT");
+var sendAssetToWallet = require("./services/sendAssetToWallet");
+var getBalance = require("./services/getBalance");
+var checkWallet = require("./services/checkWallet");
+var walletToWalletTx = require("./services/walletToWalletTx");
 //var createWallet = require('./services/createWallet')
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/generateNFT', generateNFT);
-app.use('/sendAssetToWallet', sendAssetToWallet);
-app.use('/getBalance', getBalance);
-app.use('/checkWallet', checkWallet);
-app.use('/generateTransaction', walletToWalletTx);
+app.use("/generateNFT", generateNFT);
+app.use("/sendAssetToWallet", sendAssetToWallet);
+app.use("/getBalance", getBalance);
+app.use("/checkWallet", checkWallet);
+app.use("/generateTransaction", walletToWalletTx);
 //app.use('/sendAssetToWallet', sendAssetToWallet);
 
-app.post('/healthCheck', (req, res) => {
-    console.log("healthCheck")
-    res.send('Hello World!')
-})
+app.get("/healthCheck", (req, res) => {
+  console.log("healthCheck");
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
